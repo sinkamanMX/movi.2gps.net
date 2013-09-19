1aecd13b22421e0395cb35351c5b3dcd58ae9213
Enrique Peña Gonzalez <epena@airlogisticsgps.com>
2013-09-19 18:45:57 -0500

    sdf
    sdf

---
+++ modules/mReports/Sin título 1.php
@@ -0,0 +1,201 @@
+
+
+	<table id="rspTableDet" border="0" cellpadding="0" cellspacing="0" class="pretty">
+		<thead>
+			<tr>
+				<th>Tecnologia</th>
+				<th>Area</th>
+				<th>Capacitacion Resultados</th>
+				<th>Post-Capacitacion (Mesa de Ayuda)</th>
+				<th>Indicadores de Uso Post-Capacitacion</th>
+			</tr>
+			<!--<tr>
+				<th>Programacion Logistica</th>
+				<th>Conocimiento</th>
+				<th>Competencias</th>
+				<th>CRM</th>
+				<th># Incidencias Facultador</th>
+				<th>Personal</th>
+				<th>Pacientes</th>
+				<th>Promedio</th>
+				<th>Última Semana</th>
+			</tr>-->
+		</thead>
+		<!--<tbody>
+			<tr>
+				<th rowspan="4">AbastoNET</th>
+				<td>Informacion General</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>
+				<td>10,080</td>
+				<td>1.4</td>
+				<td>3.4</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>			
+			</tr>
+			<tr>
+				<td>Operación de tecnologia</td>
+				<td>10,255</td>
+				<td>10,610</td>
+				<td>10,700</td>
+				<td>10,680</td>
+				<td>-0.2</td>
+				<td>3.7</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>			
+			</tr>
+			<tr>
+				<td>Gerencia de solución</td>
+				<td>10,255</td>
+				<td>10,610</td>
+				<td>10,700</td>
+				<td>10,680</td>
+				<td>-0.2</td>
+				<td>3.7</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>			
+			</tr>
+			<tr>
+				<td>Aborde a pacientes</td>
+				<td>10,255</td>
+				<td>10,610</td>
+				<td>10,700</td>
+				<td>10,680</td>
+				<td>-0.2</td>
+				<td>3.7</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>			
+			</tr>				
+			<tr>
+				<th rowspan="2">Portafolio Digital</th>
+				<td>Informacion General</td>
+				<td>4,625</td>
+				<td>4,365</td>
+				<td>4,445</td>
+				<td>4,675</td>
+				<td>5.2</td>
+				<td>-5.5</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>
+			</tr>
+			<tr>
+				<td>Operacion de tecnologia</td>
+				<td>3,350</td>
+				<td>3,470</td>
+				<td>3,360</td>
+				<td>3,335</td>
+				<td>3,440</td>
+				<td>3.1</td>
+				<td>2.7</td>
+				<td>9,945</td>
+				<td>9,945</td>
+			</tr>
+			<tr>
+				<th rowspan="2">MIDO</th>
+				<td>Informacion General</td>
+				<td>19,120</td>
+				<td>18,675</td>
+				<td>18,780</td>
+				<td>19,910</td>
+				<td>6.0</td>
+				<td>1.7</td>
+				<td>9,850</td>
+				<td>9,945</td>
+				<td>9,945</td>			
+			</tr>
+			<tr>
+				<td>Gerencia de MIDO</td>
+				<td>1,725</td>
+				<td>1,780</td>
+				<td>1,695</td>
+				<td>1,585</td>
+				<td>1,820</td>
+				<td>14.8</td>
+				<td>5.5</td>
+				<td>14.8</td>
+				<td>5.5</td>			
+			</tr>
+			<tr>
+				<th rowspan="1">Modulo MIDO</th>
+				<td>Operacion de tecnologia</td>
+				<td>4,750</td>
+				<td>4,620</td>
+				<td>4,580</td>
+				<td>4,880</td>
+				<td>6.6</td>
+				<td>3.0</td>
+				<td>4,880</td>
+				<td>6.6</td>
+				<td>3.0</td>			
+			</tr>
+			<tr>
+				<th rowspan="1">MIDO Portatil</th>
+				<td>Operacion de tecnologia</td>
+				<td>7,040</td>
+				<td>6,250</td>
+				<td>6,150</td>
+				<td>6,525</td>
+				<td>6.1</td>
+				<td>-10.1</td>
+				<td>4,880</td>
+				<td>6.6</td>
+				<td>3.0</td>			
+			</tr>
+			<tr>
+				<th rowspan="3">VIve Sano DIabediario</th>
+				<td>Informacion General</td>
+				<td>4,675</td>
+				<td>4,380</td>
+				<td>4,500</td>
+				<td>4,570</td>
+				<td>1.6</td>
+				<td>1.3</td>
+				<td>4,880</td>
+				<td>6.6</td>
+				<td>3.0</td>			
+			</tr>
+			<tr>
+				<td>Operación de tecnologia</td>
+				<td>6,560</td>
+				<td>6,555</td>
+				<td>6,065</td>
+				<td>6,030</td>
+				<td>6,145</td>
+				<td>1.9</td>
+				<td>-6.3</td>
+				<td>1.9</td>
+				<td>-6.3</td>			
+			</tr>
+			<tr>
+				<td>Aborde a pacientes</td>
+				<td>2,985</td>
+				<td>3,080</td>
+				<td>3,075</td>
+				<td>3,145</td>
+				<td>3,295</td>
+				<td>4.8</td>
+				<td>10.4</td>
+				<td>1.9</td>
+				<td>-6.3</td>			
+			</tr>
+			<tr>
+				<th rowspan="3">SI-CASALUD</th>
+				<td>Operación de tecnologia</td>
+				<td>5,595</td>
+				<td>5,255</td>
+				<td>5,295</td>
+				<td>5,330</td>
+				<td>0.7</td>
+				<td>-2.4</td>
+				<td>1.9</td>
+				<td>-6.3</td>			
+				<td>-6.3</td>			
+			</tr>
+		</tbody>-->
+	</table>
